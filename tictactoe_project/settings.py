754acefd4afb36e0fdebfcd082f95efe7e4598b5
Lu <117615336+lu-developer476@users.noreply.github.com>
2026-03-04 06:09:05 -0300

    Agregar modo IA con 5 niveles y mejorar UI del juego

--- tictactoe_project/settings.py
+++ tictactoe_project/settings.py
@@ -32,6 +32,13 @@ TEMPLATES = [{
 'BACKEND': 'django.template.backends.django.DjangoTemplates',
 'DIRS': [],
 'APP_DIRS': True,
+'OPTIONS': {
+'context_processors': [
+'django.template.context_processors.request',
+'django.contrib.auth.context_processors.auth',
+'django.contrib.messages.context_processors.messages',
+],
+},
 }]
 
 WSGI_APPLICATION = 'tictactoe_project.wsgi.application'
